fa897c5ac0b5c64b70aeeb9211a52fe13ec03353
hanwh <hanwhhanwh@gmail.com>
2018-11-22 22:41:00 +0900

    modify where clause.

--- Demo/mariadb_demo.js
+++ Demo/mariadb_demo.js
@@ -8,5 +8,37 @@ const net = require('net');
 const mysql = require('mysql');
 
 const config = require('../lib/config.json');
-const logger = require('../lib/logger.js');
 
+var connection = mysql.createConnection({
+	host: config.db_host_ip,
+	port: config.db_host_port,
+	user: config.db_login_id,
+	password: config.db_login_passwd,
+	database: config.db_name
+});
+
+connection.connect();
+
+let strQuery = "\
+SELECT \
+	miner_no, miner_name, miner_ip, reg_date, mod_date, miner_spec \
+FROM MINERS AS M \
+WHERE 1 = 1\
+	AND M.miner_name = ?\
+;";
+
+let objWhere = { miner_no: 2 };
+
+console.log(strQuery);
+connection.query(strQuery, ['miner #02'], function (err, rows, fields) {
+	connection.end();
+	if (!err) {
+		console.log(rows);
+		console.log(fields);
+		var result = 'rows : ' + JSON.stringify(rows) + '<br><br>' +
+			'fields : ' + JSON.stringify(fields);
+		console.log(result);
+	} else {
+		console.log('query error : ' + err);
+	}
+});
