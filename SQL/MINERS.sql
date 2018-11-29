0f1f93a8faedb417e2f508b5d06b48f332c8c5a1
hanwh <hanwhhanwh@gmail.com>
2018-11-29 17:44:05 +0900

    modify ip column : VARCHAR -> INT

--- SQL/MINERS.sql
+++ SQL/MINERS.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS `MINERS`
 (
 	`miner_no` INT NOT NULL AUTO_INCREMENT COMMENT '장비 번호'
 	, `miner_name` VARCHAR(100) NOT NULL COMMENT '장비 이름'
-	, `miner_ip` INT NOT NULL COMMENT '장비 IP'
+	, `miner_ip` INT UNSIGNED NOT NULL COMMENT '장비 IP'
 	, `reg_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '장비 등록시각'
-	, `mod_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '장비 정보 수정시각'
+	, `mod_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '장비 정보 수정시각'
 	, `miner_spec` LONGTEXT NULL DEFAULT NULL COMMENT '장비 상세 사양'
 
 	, PRIMARY KEY (`miner_no`)
@@ -16,8 +16,8 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
 
 INSERT INTO `MINERS` ( miner_name, miner_ip, reg_date, mod_date, miner_spec ) VALUES 
-		( 'miner #00', '192.168.0.100', NOW(), NOW(), NULL )
-		, ( 'miner #01', '192.168.0.101', NOW(), NOW(), NULL )
-		, ( 'miner #02', '192.168.0.102', NOW(), NOW(), NULL )
-		, ( 'miner #03', '192.168.0.103', NOW(), NOW(), NULL )
+		( 'miner #00', INET_ATON('192.168.0.100'), NOW(), NOW(), NULL )
+		, ( 'miner #01', INET_ATON('192.168.0.101'), NOW(), NOW(), NULL )
+		, ( 'miner #02', INET_ATON('192.168.0.102'), NOW(), NOW(), NULL )
+		, ( 'miner #03', INET_ATON('192.168.0.103'), NOW(), NOW(), NULL )
 		;
